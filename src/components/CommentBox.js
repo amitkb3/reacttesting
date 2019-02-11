39400c6eedecfdff5e785e7877da326cf060f197
Amit Bothra <amit.bothra@outlook.com>
2019-02-11 05:58:19 -0600

    added requireAuth Higher Order Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import requireAuth from './requireAuth';
 
 class CommentBox extends React.Component {
   state = { comment: ''};
-
+  
+  
   handleChange = event => {
     this.setState({ comment: event.target.value});
   }
@@ -41,4 +43,4 @@ class CommentBox extends React.Component {
   }  
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
